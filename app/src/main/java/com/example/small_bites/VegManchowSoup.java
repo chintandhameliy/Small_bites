e9be80c629ea8db6b47e45dabbdc5ce177cda806
Jaimish Dholariya <jdholariya374@rku.ac.in>
2022-10-08 05:59:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/small_bites/VegManchowSoup.java
@@ -0,0 +1,19 @@
+package com.example.small_bites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VegManchowSoup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_veg_manchow_soup);
+
+        if(getSupportActionBar()!= null)
+        {
+            getSupportActionBar().hide();
+        }
+    }
+}
